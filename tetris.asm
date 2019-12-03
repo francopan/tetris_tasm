651557dd7e48921b5e5b5548fa7995d096c01696
Franco Pan <francovski2@gmail.com>
2019-12-03 20:31:32 -0300

    CHanged Tetraminoes Rendering and Top-Down Animation

--- tetris.asm
+++ tetris.asm
@@ -19,22 +19,33 @@
         SCORE_MSG db 'SCORE$'
         
         ; Pecas (Nomes retirados de https://en.wikipedia.org/wiki/Tetromino)
-        PECA_S db '    $','    $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $',QUADRADO_CHAR,QUADRADO_CHAR,'  $'
-        PECA_T db '    $','    $','  ',QUADRADO_CHAR,' $',' ',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$'
-        PECA_J db '    $','    $',QUADRADO_CHAR,'   $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,' ','$'
-        PECA_O db '    $','    $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $'
-        PECA_I db '    $','    $','    $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$'
-        PECA_L db '    $','    $','   ',QUADRADO_CHAR,'$',' ',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$'
-        PECA_Z db '    $','    $',QUADRADO_CHAR,QUADRADO_CHAR,'  $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $'
-            
+        ;PECA_S db '    $','    $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $',QUADRADO_CHAR,QUADRADO_CHAR,'  $'
+        ;PECA_T db '    $','    $','  ',QUADRADO_CHAR,' $',' ',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$'
+        ;PECA_J db '    $','    $',QUADRADO_CHAR,'   $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,' ','$'
+        ;PECA_O db '    $','    $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $'
+        ;PECA_I db '    $','    $','    $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$'
+        ;PECA_L db '    $','    $','   ',QUADRADO_CHAR,'$',' ',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$'
+        ;PECA_Z db '    $','    $',QUADRADO_CHAR,QUADRADO_CHAR,'  $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $'
+
+        PECA_S db ' ',QUADRADO_CHAR,QUADRADO_CHAR,' $',QUADRADO_CHAR,QUADRADO_CHAR,'  $','    $','    $'
+        PECA_T db ' ',QUADRADO_CHAR,'  $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,' $','    $','    $'
+        PECA_J db QUADRADO_CHAR,'   $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,' ','$','    $','    $'
+        PECA_O db ' ',QUADRADO_CHAR,QUADRADO_CHAR,' $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $','    $','    $'
+        PECA_I db '    $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,'$','    $','    $'
+        PECA_L db '  ',QUADRADO_CHAR,' $',QUADRADO_CHAR,QUADRADO_CHAR,QUADRADO_CHAR,' $','    $','    $'
+        PECA_Z db QUADRADO_CHAR,QUADRADO_CHAR,'  $',' ',QUADRADO_CHAR,QUADRADO_CHAR,' $','    $','    $'
+
     
     ;  ~Variaveis ~ Jogo
     ;------------------------------------
     SCORE_JOGO db 5 dup('0'),'$'  ; Texto do Score
     SCORE_JOGO_HEX dw ?       ; Pontuacao (hex)
+    PROX_PECA_NUM db ?
+    CURR_PECA_NUM db ?
+    CURR_W db ?
+    CURR_H db ?
    
 .code
-
 
 PushAXBX macro 
     push AX 
@@ -120,83 +131,64 @@ IMP_PECA_GENERICO proc ; SI  = offset peca ; DH = linha da peca; DL = coluna da 
   ret
 endp
 
-IMP_PECA_T proc
-    push BX
-    mov SI, OFFSET PECA_T
-    mov BL, 04h
-    call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
+IMP_PECA proc ; al = peca ; ah [0 = escreve; 1 = apaga]
+    PushAXBXCXDX
+    
+    cmp  AL, 00d ; Peca T
+    jne  tenta_peca_s
+        mov BL, 04h            
+        mov SI, OFFSET PECA_T
+        jmp imprime_peca_agora
+    
+    tenta_peca_s:
+    cmp  AL, 01d ; Peca S
+    jne  tenta_peca_z
+        mov BL, 06h            
+        mov SI, OFFSET PECA_S
+        jmp imprime_peca_agora
+    
+    tenta_peca_z:    
+    cmp  AL, 02d ; Peca Z
+    jne  tenta_peca_i
+        mov BL, 0Eh            
+        mov SI, OFFSET PECA_Z
+        jmp imprime_peca_agora
 
-IMP_PECA_S proc
-    push BX
-    mov SI, OFFSET PECA_S
-    mov BL, 06h
-    call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
+    tenta_peca_i:    
+    cmp  AL, 03d ; Peca I
+    jne  tenta_peca_o
+        mov BL, 02h            
+        mov SI, OFFSET PECA_I
+        jmp imprime_peca_agora
 
-IMP_PECA_Z proc
-    push BX
-    mov SI, OFFSET PECA_Z
-    mov BL, 0Eh
-    call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
+    tenta_peca_o:    
+    cmp  AL, 04d ; Peca O
+    jne  tenta_peca_J
+        mov BL, 03h            
+        mov SI, OFFSET PECA_O
+        jmp imprime_peca_agora
 
-IMP_PECA_I proc
-    push BX
-    mov SI, OFFSET PECA_I
-    mov BL, 02h
-    call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
+    tenta_peca_J:    
+    cmp  AL, 05d ; Peca j
+    jne  tenta_peca_L
+        mov BL, 09h            
+        mov SI, OFFSET PECA_J
+        jmp imprime_peca_agora
+    
+    tenta_peca_L:
+        mov BL, 01h            
+        mov SI, OFFSET PECA_L
+        jmp imprime_peca_agora
 
-IMP_PECA_O proc
-    push BX
-    mov SI, OFFSET PECA_O
-    mov BL, 03h
+    imprime_peca_agora:
+    cmp ah, 1
+    jne pula_e_imprime
+    mov bl,0
+    pula_e_imprime:
+    mov AX,0
     call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
 
-IMP_PECA_L proc
-    push BX
-    mov SI, OFFSET PECA_L
-    mov BL, 01h
-    call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
-
-IMP_PECA_J proc
-    push BX
-    mov SI, OFFSET PECA_J
-    mov BL, 09h
-    call IMP_PECA_GENERICO
-    pop BX
-ret
-endp
-
-IMP_PECA proc ; al = peca
-    cmp  AL, 00d
-    je   IMP_PECA_T
-    cmp  AL, 01d
-    je   IMP_PECA_S
-    cmp  AL, 02d
-    je   IMP_PECA_Z
-    cmp  AL, 03d
-    je   IMP_PECA_I
-    cmp  AL, 04d
-    je   IMP_PECA_O
-    cmp  AL, 05h
-    je   IMP_PECA_L
-    jmp  IMP_PECA_J
+    PopAXBXCXDX
 ret
 endp
 
@@ -207,7 +199,10 @@ IMP_STRING proc ; Proc recebe a palavra, posicao e cor para imprimir na tela [SI
     mov  AL, [SI]       ;Aponta para o endereco na memoria da string.
     loop_imp_string:
         call GOTO_XY            ;Chama o macro que seta o cursor [x,y]
+        cmp AL, ' '
+        je pula_char_display
         call CHAR_DISPLAY    ;Chama o macro que imprime na tela o caractere colorido
+        pula_char_display:
         inc SI             ;Incremente a posicao na memoria para o proximo caractere
         inc DL              ;Incrementa a posicao x na tela para impressao do proximo caractere 
         mov AL, [SI]       ;Aponta para o endereco na memoria da string.
@@ -231,10 +226,63 @@ endp
 GERAR_PROXIMA_PECA proc
     PushAXBXCXDX
     call GENERATE_RANDOM
-    mov  DH, 02h
+    mov  DH, 04h
     mov  DL, 1Dh
     call IMP_PECA
+    mov SI, offset PROX_PECA_NUM
+    mov [SI], AL
     PopAXBXCXDX
+ret
+endp
+
+DELAY proc
+    PushAXBXCXDX
+    MOV     CX, 0FH
+    MOV     DX, 4240H
+    MOV     AH, 86H
+    INT     15H
+    PopAXBXCXDX
+ret
+endp
+
+PEGAR_POSICAO_PECA proc
+    mov SI, offset CURR_W
+    mov DH, [SI]
+    mov SI, offset CURR_H
+    mov DL, [SI]
+    mov SI, offset CURR_PECA_NUM
+    mov AL, [SI]
+ret
+endp
+
+GAMEPLAY proc
+    ; Adiciona Proxima Peca na tela
+    PushAXBXCXDX
+    mov SI, offset PROX_PECA_NUM
+    mov AL, [SI]
+    mov SI, offset CURR_PECA_NUM
+    mov [SI], AL
+    mov DL, 10h
+    mov DH, 2h
+    mov SI, offset CURR_W
+    mov [SI], DH
+    mov SI, offset CURR_H
+    mov [SI], DL
+    call IMP_PECA
+    PopAXBXCXDX
+    
+    call GERAR_PROXIMA_PECA
+
+    call PEGAR_POSICAO_PECA
+    mov CX,20d
+    loop_cai_peca:
+        call DELAY
+        mov AH,1
+        call IMP_PECA
+        inc DH
+        mov AH,0
+        call IMP_PECA
+    loop loop_cai_peca
 ret
 endp
 
@@ -297,12 +345,11 @@ TELA_JOGO proc
 
     ; Imprime Primeira Peca
     call GERAR_PROXIMA_PECA
-
+    call GAMEPLAY
     PopAXBXCXDX
     ret
 endp
 
-  
 TELA_INICIAL proc
    PushAXBXCXDX
    call LIMPAR_TELA
@@ -315,57 +362,40 @@ TELA_INICIAL proc
    call IMP_STRING
   
    ; Exibe Tetraminos
-   mov DH, 06h
-   mov DL, 06h
-   mov AL, 00h
-   call IMP_PECA ; Imprime J
+   mov DH, 09h ; Linha inicial
+   mov DL, 05h ; Coluna inicial
+   mov AX, 00h ; Tetramino inicial
+   mov CX, 7
+   loop_tetramino_menu:
+    call IMP_PECA
+    add DL, 04d
+    inc AL
+   loop loop_tetramino_menu
    
-   mov DL, 0Bh
-   inc AL
-   call IMP_PECA   
-   
-   mov DL, 0Fh
-   inc AL
-   call IMP_PECA
-   
-   mov DL, 13h
-   inc AL
-   call IMP_PECA
-   
-   mov DL, 17h
-   inc AL
-   call IMP_PECA
-   
-   mov DL, 1Eh
-   inc AL
-   call IMP_PECA
-   
-   mov DL, 1Bh
-   inc AL
-   call IMP_PECA
-   
-   mov SI, offset JOGAR ; Opcao de Jogar
+   ; Opcao de Jogar
+   mov SI, offset JOGAR 
    mov BL, 0Fh
    mov DH, 0Fh 
    mov DL, 0Fh ; 15d
    call IMP_STRING
-  
-   mov SI, offset SAIR ; Opcao de Sair
+   
+   ; Opcao de Sair
+   mov SI, offset SAIR 
    mov DH, 11h ;17d
    mov DL, 0Fh ; 15d
    call IMP_STRING
-
-   mov SI, offset DESENV ; Mensagem Desenvolvedores
+   
+   ; Mensagem Desenvolvedores
+   mov SI, offset DESENV 
    mov BL, 04h
    mov DH, 016h ;22d
-   mov DL, 01h 
+   mov DL, 0h
    call IMP_STRING
    
    ; Realiza a leitura do usuario para entrar ou sair do jogo
    LEITURA_MENU:
        mov AX, 0h
        int 16h 
-        
        cmp    AL, 'J'
        je     COMECA_JOGO
        cmp    AL, 'j'
